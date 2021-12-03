be0433010ee9dd29fd7bcb78619ef03f554719ba
nick <nickyoung3300@gmail.com>
2021-12-03 18:13:27 +0800

    signUp_註冊頁面

--- pr-2logintest.php
+++ pr-2logintest.php
@@ -8,9 +8,8 @@
     <title>Document</title>
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-</head>
-<style>
-    body {
+    <style>
+            body {
         margin-bottom: 0px;
     }
 
@@ -18,32 +17,107 @@
         font-size: 50px;
     }
 
+    .dropbtn {
+        background-color: #3498DB;
+        color: white;
+        padding: 16px;
+        font-size: 16px;
+        border: none;
+        cursor: pointer;
+    }
 
-</style>
+    .dropbtn:hover,
+    .dropbtn:focus {
+        background-color: #2980B9;
+    }
+
+    .dropdown {
+        position: relative;
+        display: inline-block;
+    }
+
+    .dropdown-content {
+        display: none;
+        position: absolute;
+        background-color: #f1f1f1;
+        min-width: 150px;
+        overflow: auto;
+        box-shadow: 0px 8px 16px 0px rgba(0, 0, 0, 0.2);
+        z-index: 1;
+    }
+
+    .dropdown-content a {
+        color: black;
+        padding: 12px 16px;
+        text-decoration: none;
+        display: block;
+    }
+
+    .dropdown a:hover {
+        background-color: #ddd;
+    }
+
+    .show {
+        display: block;
+    }
+    </style>
+</head>
 
 <body>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">首頁</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#"> </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="./pr-3Show.php" style="font-size:large;    color:deeppink">顯示所有商品</a>
+                        
+                    </li>
+                   
 
-
-
-    <div class="container" style="text-align: center;">
-
-
-            <div class="header1">自然生態</div>
+                </ul>
+                <form class="d-flex" action="./pr-3search.php" method="post">
+                    <input class="form-control me-2" name="search" type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-outline-success" type="submit">Search</button>
+                </form>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        登入 / 註冊
+                    </a>
+                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <li><a class="dropdown-item" href="#">登入</a></li>
+                        <li><a class="dropdown-item" href="./pr-2signUp.php">註冊</a></li>
+                        <li>
+                            <hr class="dropdown-divider">
+                        </li>
+                        <li><a class="dropdown-item" href="#">查詢帳密</a></li>
+                    </ul>
+                </li>
+            </div>
             
+        </div>
+    </nav>
 
-        
 
-    </div>
+
+
     <div class="container" style="text-align: center ; font-size:50px;">
     <?php
     $firstName=$_POST["firstname"];
     $lastName=$_POST["lastname"];
     $county=$_POST["county"];
     $district=$_POST["district"];
-    echo"<h2>"."歡迎".$lastName."先生/女士"."</h2>";
+    $pwd=$_POST["pwd"];
+    $email=$_POST["email"];
+    echo"註冊成功";
     echo"<h2>"."歡迎".$firstName."</h2>";
-    echo"<h2>"."歡迎居住在".$county."縣/市"."的".$district."</h2>";
-
+    
 
 
 
--- pr-2signUp.php
+++ pr-2signUp.php
@@ -10,15 +10,38 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 </head>
 <style>
-    body {
-        margin-bottom: 0px;
-    }
+body {font-family: Arial, Helvetica, sans-serif;}
+* {box-sizing: border-box;}
 
-    .header1 {
-        font-size: 50px;
-    }
+input[type=text], select, textarea {
+  width: 100%;
+  padding: 12px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  box-sizing: border-box;
+  margin-top: 6px;
+  margin-bottom: 16px;
+  resize: vertical;
+}
 
+input[type=submit] {
+  background-color: #04AA6D;
+  color: white;
+  padding: 12px 20px;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+}
 
+input[type=submit]:hover {
+  background-color: #45a049;
+}
+
+.container {
+  border-radius: 5px;
+  background-color: #f2f2f2;
+  padding: 20px;
+}
 </style>
 
 <body>
@@ -27,24 +50,25 @@
 
     <div class="container" style="text-align: center;">
 
-
-            <div class="header1">自然生態</div>
+        <h1>會員註冊</h1>
+           
             
 
         
 
     </div>
-    <div class="container" style="text-align: center ; font-size:50px;">
+    <div class="container" >
       <form action="./pr-2logintest.php" method="post">
-        姓&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;氏:&nbsp;<input type="text" name="lastname"><br>
-        名&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;子:&nbsp;<input type="text"name="firstname"><br>
-        縣&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;市:&nbsp;<input type="text"name="county"><br>
-        鄉鎮市區:&nbsp;<input type="text"name="district"><br>
-        password:<input type="text"name="pws"><br>
-        E-mail&nbsp;:&nbsp;&nbsp;&nbsp;&nbsp;<input type="text"name="pws"><br>
+        姓&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;氏&nbsp;<input type="text" name="lastname"><br>
+        名&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;子&nbsp;<input type="text"name="firstname"><br>
+        縣&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;市&nbsp;<input type="text"name="county"><br>
+        鄉鎮市區&nbsp;<input type="text"name="district"><br>
+        password<input type="text"name="pwd"><br>
+        E-mail&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type="text"name="email"><br>
         <input type="submit">   
 
       </form>
+
 
     </div>
 
