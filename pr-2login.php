43c5409f42f36f48def93e9a5c7df97d5b6fd484
nick <nickyoung3300@gmail.com>
2021-12-03 15:42:39 +0800

    HomePage_img1

--- pr-2login.php
+++ pr-2login.php
@@ -22,6 +22,45 @@
 </style>
 
 <body>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">首頁</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#"> </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="./pr-3Show.php" style="font-size:large;    color:deeppink">顯示所有商品</a>
+                        
+                    </li>
+                   
+
+                </ul>
+                <form class="d-flex" action="./pr-3search.php" method="post">
+                    <input class="form-control me-2" name="search" type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-outline-success" type="submit">Search</button>
+                </form>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        登入 / 註冊
+                    </a>
+                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <li><a class="dropdown-item" href="#">登入</a></li>
+                        <li><a class="dropdown-item" href="./pr-2signUp.php">註冊</a></li>
+                        <li>
+                            <hr class="dropdown-divider">
+                        </li>
+                        <li><a class="dropdown-item" href="#">查詢帳密</a></li>
+                    </ul>
+                </li>
+            </div>
+            
+        </div>
+    </nav>
 
 
 
