a3b5191db3905a6b9045af2c902de56c81ddd41a
nick <nickyoung3300@gmail.com>
2021-12-09 17:46:55 +0800

    header_header優化

--- pr-2login.php
+++ pr-2login.php
@@ -66,7 +66,7 @@
 <body>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <div class="container-fluid">
-            <a class="navbar-brand" href="#">首頁</a>
+            <a class="navbar-brand" href="pr-2.php">首頁</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
--- pr-3Show.php
+++ pr-3Show.php
@@ -12,10 +12,142 @@
         table tr td {
             border: solid 1px black;
         }
+        <style>
+    body {
+        margin-bottom: 0px;
+    }
+
+    .header1 {
+        font-size: 50px;
+    }
+
+    .dropbtn {
+        background-color: #3498DB;
+        color: white;
+        padding: 16px;
+        font-size: 16px;
+        border: none;
+        cursor: pointer;
+    }
+
+    .dropbtn:hover,
+    .dropbtn:focus {
+        background-color: #2980B9;
+    }
+
+    .dropdown {
+        position: relative;
+        display: inline-block;
+    }
+
+    .dropdown-content {
+        display: none;
+        position: absolute;
+        background-color: #f1f1f1;
+        min-width: 150px;
+        overflow: auto;
+        box-shadow: 0px 8px 16px 0px rgba(0, 0, 0, 0.2);
+        z-index: 1;
+    }
+
+    .dropdown-content a {
+        color: black;
+        padding: 12px 16px;
+        text-decoration: none;
+        display: block;
+    }
+
+    .dropdown a:hover {
+        background-color: #ddd;
+    }
+
+    .show {
+        display: block;
+    }
+</style>
     </style>
 </head>
 
 <body>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="pr-2.php">首頁</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#"> </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="./pr-3Show.php" style="font-size:large;    color:deeppink">商品管理</a>
+                        
+                    </li>
+                   
+
+                </ul>
+                <form class="d-flex" action="./pr-3search.php" method="post">
+                    <input class="form-control me-2" name="search" type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-outline-success" type="submit">Search</button>
+                </form>&nbsp; &nbsp; 
+                <li class="nav-item dropdown">
+
+                    <?php
+                   
+
+                    if(isset($_COOKIE['passed'])){
+                        
+                        echo'<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" 
+                                       role="button" data-bs-toggle="dropdown" aria-expanded="false">'
+                                        .$_COOKIE['name'].'</a>';
+                        echo'<ul class="dropdown-menu" aria-labelledby="navbarDropdown">';
+                        echo'<li><a class="dropdown-item" href="./pr-2login.php  ">用戶管理</a></li>';
+                        echo'<li><a class="dropdown-item" href="./pr-2login.php  ">商品管理</a></li>';
+                       
+                        echo'<li>
+                        <hr class="dropdown-divider">
+                        </li>';
+                        echo'<li><a class="dropdown-item" href="logout.php">登出</a></li>';
+                        echo'</ul>';
+                    }else{
+                        
+                        echo'<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        登入 / 註冊
+                    </a>';
+                        echo'<ul class="dropdown-menu" aria-labelledby="navbarDropdown">';
+                        echo'<li><a class="dropdown-item" href="./pr-2login.php  ">登入</a></li>';
+                        echo'<li><a class="dropdown-item" href="./pr-2signUp.php">註冊</a></li>';
+                        echo'<li>
+                        <hr class="dropdown-divider">
+                    </li>';
+                        echo'<li><a class="dropdown-item" href="searchPwd.php">查詢帳密</a></li>';
+                        echo'</ul>';
+                  
+                    }
+
+
+
+
+                    ?>
+
+
+                    <!-- <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        登入 / 註冊
+                    </a>
+                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <li><a class="dropdown-item" href="./pr-2login.php  ">登入</a></li>
+                        <li><a class="dropdown-item" href="./pr-2signUp.php">註冊</a></li>
+                        <li>
+                            <hr class="dropdown-divider">
+                        </li>
+                        <li><a class="dropdown-item" href="#">查詢帳密</a></li>
+                    </ul> -->
+                </li>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; 
+            </div>
+            
+        </div>
+    </nav>  
     <div class="container" style="text-align: center;">
         <!-- CRUD-R 連線資料庫讀取   -->
         <?php
